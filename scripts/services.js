2c1e9c28d3824681fbd4125b4840b31825d9f59b
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-09-02 22:03:16 -0600

    Sidebar responsive design

--- scripts/services.js
+++ scripts/services.js
@@ -16,3 +16,17 @@ function pause() {
   document.getElementById("audioIconON").id="audioIconOFF";
   document.getElementById("audioON").id="audioOFF";
 }
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("leftNavContainer").style.display = "flex";
+  document.getElementById("openBtn").style.display = "none";
+  document.getElementById("fatherContainer2").style.marginLeft = "7%";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("leftNavContainer").style.display = "none";
+  document.getElementById("openBtn").style.display = "block";
+  document.getElementById("fatherContainer2").style.marginLeft = "0%";
+}
