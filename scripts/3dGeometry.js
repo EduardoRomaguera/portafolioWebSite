5c280e8d8d408f58e16a9f5e08f8fbfb5bdbb780
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-09-06 15:59:53 -0600

    Clean code of grometry1

--- scripts/3dGeometry.js
+++ scripts/3dGeometry.js
@@ -1,15 +1,5 @@
 // Imports
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-// import './style.css'
-// import * as THREE from 'three'
-// import * as THREE from 'https://cdn.skypack.dev/three@v0.132.2-1edwuDlviJO0abBvWgKd/mode=imports/optimized/three.js';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-// import * as dat from 'dat.gui'
-// import { PointLight, Sphere } from 'three'
-
-
-// Visual controls
-// const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -22,7 +12,6 @@ const geometry = new THREE.IcosahedronGeometry(5, 0);
 const geometry2 = new THREE.IcosahedronGeometry(0.9, 0);
 const geometry3 = new THREE.IcosahedronGeometry(0.9, 0);
 const geometry4 = new THREE.IcosahedronGeometry(0.2, 0);
-
 
 // Loading
 const textureLoader = new THREE.TextureLoader()
@@ -75,12 +64,6 @@ scene.add(point1)
 scene.add(point2)
 scene.add(point3)
 
-
-// Fog
-// const fog = new THREE.Fog(0xb5b5b5, 1, 20);
-// // const fog = new THREE.FogExp2(0xa83232, 0.0005);
-// scene.add(fog)
-
 // Lights
 //RED
 const pointLight1 = new THREE.PointLight(0xc22d2d, 2)
@@ -110,10 +93,10 @@ scene.add(pointLight5)
 const spotLight = new THREE.SpotLight( 0x8e4dff );
 spotLight.position.set( 0, 0, 30 );
 spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 512; // default
-spotLight.shadow.mapSize.height = 512; // default
-spotLight.shadow.camera.near = 0.5; // default
-spotLight.shadow.camera.far = 500; // default
+spotLight.shadow.mapSize.width = 1024; // default
+spotLight.shadow.mapSize.height = 1024; // default
+spotLight.shadow.camera.near = 10; // default
+spotLight.shadow.camera.far = -10; // default
 spotLight.shadow.focus = 1; // default
 scene.add( spotLight );
 
@@ -158,11 +141,6 @@ camera.position.y = 0
 camera.position.z = 24
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
@@ -170,6 +148,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
 // Animate
 document.addEventListener('mousemove', onDocumentMouseMove)
@@ -200,8 +180,8 @@ const tick = () =>
     const num2 = Math.random()
     sphere.rotation.y = 0.4 * elapsedTime
     sphere.rotation.x = .4 * elapsedTime
-    sphere.rotation.y += .4 * (targetX - sphere.rotation.y)
-    sphere.rotation.x += .4 * (targetY - sphere.rotation.x)
+    sphere.rotation.y += .4 * (targetX - sphere.rotation.x)
+    sphere.rotation.x += .4 * (targetY - sphere.rotation.y)
 
     point1.rotation.x = sphere.rotation.x
     point1.rotation.y = sphere.rotation.y
